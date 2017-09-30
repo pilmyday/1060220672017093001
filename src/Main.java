28cb4632e2c533e33f997d31c20a3d51abed67b8
pilmyday <roy00374@mail.com>
2017-09-30 15:43:16 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int x,y;
+        x=in.nextInt();
+        y=in.nextInt();
+        System.out.printf("和=%d\n",x+y);
+        System.out.printf("差=%d\n",x-y);
+        System.out.printf("乘積=%d\n",x*y);
+
+
+
+    }
+}
